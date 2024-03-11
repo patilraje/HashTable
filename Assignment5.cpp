c69c4bedfec5d7630d5c67c66ceb192e41e5005b
patilraje <126375795+patilraje@users.noreply.github.com>
2024-03-10 17:51:24 -0700

    Update Assignment5.cpp

--- Assignment5.cpp
+++ Assignment5.cpp
@@ -1,6 +1,7 @@
+
 // ASU CSE310 Assignment #5 Spring 2024
-// Name of Author: Raje Patil
-// ASU ID: 1231031453
+// Name of Author:
+// ASU ID:
 // Description: this is the main program that reads input from a text file,
 // it then call hash functions to execute hash commands given in the input.
 // ---- is where you need to add your own code
@@ -15,159 +16,147 @@
 ********************************************************************************/
 
 #include "Hash.h"
+#include <sstream>
 #include <iostream>
 #include <iomanip>
 #include <string>
-#include <sstream>
 
 using namespace std;
 
-//This function used to get a Car key which is the combination of model, make and vin
+// This function is used to get a Car key, which is the combination of model, make, and vin
+// Function declarations
+void extractCarInfo(string oneLine, string& model, string& make, int& vin, double& price);
 void getKey(string oneLine, string& model, string& make, int& vin);
-//this function for price too
-void getKeyTwo(string oneLine, string& model, string& make, int& vin, double& price);
 
 int main()
 {
-   int size = 0 ;
+   int size = 0;
    int numOfCommand = 0;
-   string model, make, oneLine;  //added oneLine
+   string model, make, command, oneLine;
    int vin;
-   double price;
-   string command;   //newly added string
+   double price= 0.0;
 
-   //declare any other necessary variables here
-
+   // Declare any other necessary variables here
 
    cout << "Enter the size of the hash table: ";
    cin >> size;
    cin.ignore(20, '\n');
 
-   //Instantiate the hash table with the relevant number of slots
-   //RECHECK
-  Hash* hashTable = new Hash(size);
+   // Instantiate the hash table with the relevant number of slots
+   Hash* hashTable = new Hash(size);
 
-   //use this do..while loop to repeatedly get one line Car info. and extract tokens
-   //create one Car object and insert it inside the hashTable until seeing the message
-   //"InsertionEnd" to terminate
+   // Use this do-while loop to repeatedly get one line of Car info and extract tokens
+   // Create one Car object and insert it inside the hashTable until seeing the message
+   // "InsertionEnd" to terminate
    do
    {
-	   getline(cin,oneLine);
+      getline(cin, oneLine);
 
-
-	   if(oneLine == "InsertionEnd")
-	   {
-		   break;
-	   }
-
-	   getKeyTwo(oneLine, model, make, vin, price);  //extract the model make and vin and price, this functtion get key
-	   	   	   	   	   	   	   	   	   	   	   	   	 //is for insertion
-	   hashTable->hashInsert(model, make, vin, price); // add the car to the table
-
-
-   } while(true);
-
-
-   	  //cout << "\nEnter number of commands: ";   //***need to comment out in submitting
-      cin >> numOfCommand;
-      cin.ignore(20, '\n');
-
-
-
-      for(int i= 0; i < numOfCommand; i++)
+      if (oneLine == "InsertionEnd")
       {
-      	//get one line command, extract the first token, if only one token
-   	  cin >> command;  ///getting command for as long as the command goes??
+         break;
+      }
 
-         if(command.compare("hashDisplay") == 0)   //if(firstToken.compare("hashDisplay") == 0)
+      //for insert in particular
+      extractCarInfo(oneLine, model, make, vin, price);
+
+      // Insert the car into the hash table
+      hashTable->hashInsert(model, make, vin, price);
+
+   } while (true);
+
+   // cout << "\nEnter number of commands: ";   //***need to comment out in submitting
+   cin >> numOfCommand;
+   cin.ignore(20, '\n');
+
+   for (int i = 0; i < numOfCommand; i++)
+   {
+
+      cin >> command;
+
+      // get one line command, extract the first token, if only one token
+      if (command.compare("hashDisplay") == 0)
+      {
+         hashTable->hashDisplay();
+      }
+
+      else  // more than one token, check the command name, extract the remaining tokens
+      {
+         if (command.compare("hashSearch") == 0)
          {
-            hashTable->hashDisplay();
-            //hashTable->~Hash();
-            break;
+            // Extract model, make, and vin
+            hashTable->hashSearch(model, make, vin);
+         }
+         else if (command.compare("hashDelete") == 0)
+         {
+            // Extract model, make, and vin
+            hashTable->hashDelete(model, make, vin);
          }
 
-         else  //more than one tokens, check the command name, extract the remaining tokens
+         else if (command.compare("hashLoadFactor") == 0)
          {
-
-            if(command.compare("hashSearch")==0)
-            {
-            	hashTable->hashSearch(model, make, vin);
-            }
-
-            else  if(command.compare("hashDelete")==0)
-            {
-            	hashTable->hashDelete(model, make, vin);  //calls the delete function in hash header file
-            }
-
-            else if(command.compare("hashLoadFactor")==0)
-            {
-				 hashTable->hashLoadFactor();  //calling load factor function in hash header file
-			}
-
-            else
-            {
-           	 cout<<"Invalid command"<<endl;
-            }
+            hashTable->hashLoadFactor();
          }
-      } //end for loop
-      return 0;
+
+         else
+         {
+            cout << "Invalid command" << endl;
+         }
+      }
+   } // end for loop
+
+   delete[] hashTable; // Free the allocated memory
+   return 0;
 }
 
-
-
-//****************************************************************************************
-//Given one line, this function extracts the model, make, vin info. of a Car
-//This function is completed and given here as a study guide for extracting tokens
-void getKey(string oneLine, string& model, string& make, int& vin)
+// Given one line, this function extracts the model, make, vin info. of a Car
+void getKey(string oneLine, string& model, string& make, int& vin, int& price)
 {
    string delimiter = ","; // @suppress("Invalid arguments")
-   int pos=oneLine.find(delimiter);
-   string token = oneLine.substr(0,pos);
+   int pos = oneLine.find(delimiter);
+   string token = oneLine.substr(0, pos);
    string command = token;
-   oneLine.erase(0, pos+delimiter.length());
+   oneLine.erase(0, pos + delimiter.length());
 
-   pos=oneLine.find(delimiter);
-   token = oneLine.substr(0,pos);
+   pos = oneLine.find(delimiter);
+   token = oneLine.substr(0, pos);
    model = token;
-   oneLine.erase(0, pos+delimiter.length());
+   oneLine.erase(0, pos + delimiter.length());
 
-   pos=oneLine.find(delimiter);
-   token = oneLine.substr(0,pos);
+   pos = oneLine.find(delimiter);
+   token = oneLine.substr(0, pos);
    make = token;
-   oneLine.erase(0, pos+delimiter.length());
+   oneLine.erase(0, pos + delimiter.length());
 
-   pos=oneLine.find(delimiter);
-   token = oneLine.substr(0,pos);
+   pos = oneLine.find(delimiter);
+   token = oneLine.substr(0, pos);
    vin = stoi(token);
-   oneLine.erase(0, pos+delimiter.length());
+   oneLine.erase(0, pos + delimiter.length());
+
+   pos = oneLine.find(delimiter);
+   token = oneLine.substr(0, pos);
+   price = stod(token);
+   oneLine.erase(0, pos + delimiter.length());
+
 }
 
-void getKeyTwo(string oneLine, string& model, string& make, int& vin, double& price)
+
+// Function to extract all necessary information for adding cars
+void extractCarInfo(string oneLine, string& model, string& make, int& vin, double& price)
 {
-   string delimiter = ","; // @suppress("Invalid arguments")
-   int pos=oneLine.find(delimiter);
-   string token = oneLine.substr(0,pos);
-   string command = token;
-   oneLine.erase(0, pos+delimiter.length());
+    string delimiter = ","; // @suppress("Invalid arguments")
+    int pos = oneLine.find(delimiter);
+    model = oneLine.substr(0, pos);
+    oneLine.erase(0, pos + delimiter.length());
 
-   pos=oneLine.find(delimiter);
-   token = oneLine.substr(0,pos);
-   model = token;
-   oneLine.erase(0, pos+delimiter.length());
+    pos = oneLine.find(delimiter);
+    make = oneLine.substr(0, pos);
+    oneLine.erase(0, pos + delimiter.length());
 
-   pos=oneLine.find(delimiter);
-   token = oneLine.substr(0,pos);
-   make = token;
-   oneLine.erase(0, pos+delimiter.length());
+    pos = oneLine.find(delimiter);
+    vin = stoi(oneLine.substr(0, pos));
+    oneLine.erase(0, pos + delimiter.length());
 
-   pos=oneLine.find(delimiter);
-   token = oneLine.substr(0,pos);
-   vin = stoi(token);
-   oneLine.erase(0, pos+delimiter.length());
-
-   pos=oneLine.find(delimiter);
-   token = oneLine.substr(0,pos);
-   price = stoi(token);
-   oneLine.erase(0, pos+delimiter.length());
+    pos = oneLine.find(delimiter);
+    price = stod(oneLine.substr(0, pos));
 }
-
